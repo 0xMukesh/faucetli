704445bef1f76d0378323cd78c12097a51b9c49c
Kira272921 <sunset272921@gmail.com>
2022-04-03 17:12:03 +0530

    :package: feat: v0.0.2

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -4,8 +4,8 @@ const constants = {
   configFilePath: `${os.homedir()}/.config/faucetli/config.json`,
   configFolderPath: `${os.homedir()}/.config/faucetli`,
   ratelimitFilePath: `${os.homedir()}/.config/faucetli/ratelimit.json`,
-  ratelimitDuration: 60000,
-  ratelimitDurationLocale: "1 minute",
+  ratelimitDuration: 3600000 * 3,
+  ratelimitDurationLocale: "3 hours",
 };
 
 export default constants;
