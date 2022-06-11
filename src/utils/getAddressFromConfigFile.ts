df5bcde79aac945e0099497c04eb69fc626baf46
Jordan Baron <jordannbaron@gmail.com>
2022-06-10 22:37:36 -0400

    return address based on specified blockchain

--- src/utils/getAddressFromConfigFile.ts
+++ src/utils/getAddressFromConfigFile.ts
@@ -2,10 +2,12 @@ import fs from "fs";
 
 import constants from "../data/constants";
 
-const getAddressFromConfigFile = () => {
-  const configFile = fs.readFileSync(constants["configFilePath"], "utf8");
-  const config = JSON.parse(configFile);
-  return config.address;
+const getAddressFromConfigFile = (blockchain: string = "ethereum") => {
+  if (fs.existsSync(constants["configFilePath"])) {
+    const configFile = fs.readFileSync(constants["configFilePath"], "utf8");
+    const config = JSON.parse(configFile);
+    return blockchain === "ethereum" ? config.address : config.solAddress;
+  }
 };
 
 export default getAddressFromConfigFile;
