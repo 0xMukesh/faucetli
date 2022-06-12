66c5649f78d0bf6af6e24a40668f77a6c81e9dc5
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-06-12 18:09:02 +0530

    feat(funding,request): add funding details, added a mock print to request

--- src/core/commands/funding.rs
+++ src/core/commands/funding.rs
@@ -1,8 +1,21 @@
 use crate::cli::app::App;
 use anyhow::Result;
 use clap::Values;
+use colored::Colorize;
 
 pub fn funding(app: App, _args: Option<Values>) -> Result<()> {
-    println!("funding");
+    const DONATIONS_ADDRESS: &str = "0x894b6579524c61b56B3aB5624740464EaDF5237E";
+
+    println!(
+        "🤩 If you found {} useful and want to keep it going, we accept donations on this address: {}",
+        "faucetli".cyan(),
+        DONATIONS_ADDRESS.blue().bold()
+    );
+    println!(
+        "🔗 Link: {}{}",
+        "https://rinkeby.etherscan.io/address/".blue().bold(),
+        DONATIONS_ADDRESS.blue().bold()
+    );
+
     Ok(())
 }
--- src/core/commands/request.rs
+++ src/core/commands/request.rs
@@ -1,9 +1,20 @@
 use crate::cli::app::App;
 use anyhow::Result;
 use clap::Values;
+use colored::Colorize;
+use inquire::Text;
 
 pub fn request(app: App, args: Option<Values>) -> Result<()> {
-    println!("request");
-    println!("{:?}", args);
+    let mut args = args.unwrap().into_iter();
+    let chain = args.next().unwrap();
+
+    let wallet_address = Text::new("Enter ur wallet address ser:").prompt().unwrap();
+
+    println!(
+        "Sending tokens to {} on {} (ps: still not functioniong, just making UI lol)",
+        wallet_address.green(),
+        chain.cyan()
+    );
+
     Ok(())
 }
